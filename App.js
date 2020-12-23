bacc6558ac63e5899007be4d2fee43c66ed599a9
devjeremyjake <devjeremyjake@gmail.com>
2020-12-23 18:39:24 +0100

    creating of welcome screen

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Text
+				style={{
+					fontSize: 20,
+					fontWeight: 'bold',
+					textAlign: 'center',
+					color: 'orange',
+				}}
+			>
+				I love React Native!!!!
+			</Text>
+			<MaterialCommunityIcons name="email" size={90} color="tomato" />
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
