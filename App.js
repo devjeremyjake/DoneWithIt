1551bd0c76e8afcca4712dace07e4fd0cd499a58
devjeremyjake <devjeremyjake@gmail.com>
2020-12-24 14:58:30 +0100

    updated welcome screen and stled its buttons

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}
-		>
-			<Text
-				style={{
-					fontSize: 20,
-					fontWeight: 'bold',
-					textAlign: 'center',
-					color: 'orange',
-				}}
-			>
-				I love React Native!!!!
-			</Text>
-			<MaterialCommunityIcons name="email" size={90} color="tomato" />
-		</View>
-	);
+	return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,20 +1,24 @@
-import React from "react";
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import AppButton from '../components/AppButton';
 
-import colors from "../config/colors";
+import colors from '../config/colors';
 
 function WelcomeScreen() {
 	return (
 		<ImageBackground
+			blurRadius={2}
 			style={styles.background}
-			source={require("../assets/background.jpg")}
+			source={require('../assets/background.jpg')}
 		>
 			<View style={styles.logoContainer}>
-				<Image style={styles.logo} source={require("../assets/logo-red.png")} />
-				<Text>Sell What you dont need</Text>
+				<Image style={styles.logo} source={require('../assets/logo-red.png')} />
+				<Text style={styles.tagline}>Sell What You Don't Need</Text>
 			</View>
-			<View style={styles.loginButton}></View>
-			<View style={styles.registerButton}></View>
+			<View style={styles.buttonContainer}>
+				<AppButton title="Login" />
+				<AppButton title="Register" color="secondary" />
+			</View>
 		</ImageBackground>
 	);
 }
@@ -22,27 +26,26 @@ function WelcomeScreen() {
 const styles = StyleSheet.create({
 	background: {
 		flex: 1,
-		justifyContent: "flex-end",
-		alignItems: "center",
+		justifyContent: 'flex-end',
+		alignItems: 'center',
 	},
 	logoContainer: {
-		position: "absolute",
+		position: 'absolute',
 		top: 70,
-		alignItems: "center",
+		alignItems: 'center',
 	},
 	logo: {
 		width: 100,
 		height: 100,
 	},
-	loginButton: {
-		width: "100%",
-		height: 70,
-		backgroundColor: colors.primary,
+	buttonContainer: {
+		padding: 20,
+		width: '100%',
 	},
-	registerButton: {
-		width: "100%",
-		height: 70,
-		backgroundColor: colors.secondary,
+	tagline: {
+		fontSize: 25,
+		fontWeight: '600',
+		paddingVertical: 20,
 	},
 });
 
