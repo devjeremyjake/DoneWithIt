ead702d47a6a0382ac1c1d0e7ca36aa027f20064
devjeremyjake <devjeremyjake@gmail.com>
2020-12-25 15:51:08 +0100

    created reuasble components and new screens

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import AppButton from './app/components/AppButton';
 
+import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 export default function App() {
-	return <WelcomeScreen />;
+	return <ListingDetailsScreen />;
 }
